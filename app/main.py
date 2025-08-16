226c328bb787d9dd8552727babe42d0ca73c229e
Yehor <gamersirius053@gmail.com>
2025-08-16 13:03:48 +0300

    Решение задачи

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    res_lst = {}
+    def inner(*args, **kwargs) -> None:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in res_lst:
+            print("Getting from cache")
+            return res_lst[key]
+        elif key not in res_lst:
+            res = func(*args, **kwargs)
+            res_lst[key] = res
+            print("Calculating new result")
+            return res
+    return inner
+

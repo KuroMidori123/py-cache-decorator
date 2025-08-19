17f5adfab27b3ba12e9fbd592637e8cee1d8315e
Yehor <gamersirius053@gmail.com>
2025-08-19 21:13:04 +0300

    Решение задачи (надеюсь последнее1)

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from typing import Callable
 from functools import wraps
 
+
 def cache(func: Callable) -> Callable:
     cache_dict = {}
-
 
     @wraps(func)
     def inner(*args, **kwargs) -> None:
